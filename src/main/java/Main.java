1363cccb12f90a879dbddb1243a846b504d0aaff
Jordan Hanson <jordanmercedes22@gmail.com>
2022-01-16 16:10:14 -0700

    Built out CashDrawer object with properties and methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,21 @@
 public class Main {
         public static void main(String[] args){
                 System.out.println("ready");
+                // TODO update the property access modifiers of each class object
+
+                //Build a CashDrawer Object and Define Default values of 0
+                CashDrawer cashDrawer = new CashDrawer(0, 0, 0, 0, 0);
+
+                // Set the first Value to Test
+                int dollarAmount = 10;
+                cashDrawer.setTwenty(dollarAmount);
+
+                // Update the Total Property on the CashDrawer Object
+                cashDrawer.setTotal();
+
+                //
+                String showOutput = cashDrawer.toString();
+                System.out.println(showOutput);
+
         }
 }
